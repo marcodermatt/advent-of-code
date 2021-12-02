93c5b0d6b456bd62c7a0b9a25ef754fcc037179b
Marc Odermatt <marc.odermatt@usz.ch>
2021-12-02 11:46:23 +0100

    Relative Path

--- day-1/task-1.py
+++ day-1/task-1.py
@@ -1,10 +1,8 @@
-if __name__ == '__main__':
-
-    with open("day-1/input.txt") as f:
-        previous = 700
-        counter = 0
-        for line in f:
-            if previous < int(line):
-                counter += 1
-            previous = int(line)
-        print(counter)
+with open("input.txt") as f:
+    previous = 700
+    counter = 0
+    for line in f:
+        if previous < int(line):
+            counter += 1
+        previous = int(line)
+    print(counter)
