291c4e2a1fbd093066fa2d422ddd346ce9e707a6
Marc Odermatt <marc.odermatt@usz.ch>
2021-12-26 18:56:58 +0100

    Day 14 task 2

--- day-14/task-2.py
+++ day-14/task-2.py
@@ -1,32 +1,21 @@
-from collections import defaultdict, Counter
-
-def substitute_and_count(depth):
-    pair = ''.join(stack[:2])
-    if pair in substitutions:
-        if depth < 40:
-            char = substitutions.get(pair)
-            stack.insert(1, char)
-            counts.update({char: counts[char] + 1})
-            substitute_and_count(depth + 1)
-            substitute_and_count(depth + 1)
-        else:
-            stack.pop(0)
-    else:
-        stack.pop(0)
-
 with open("input.txt") as f:
     template = f.readline().strip()
     f.readline()
     substitutions = {line.split()[0]: line.split()[2] for line in f.readlines()}
 
-stack = list(template)
-depth = [0]
-counts = defaultdict(lambda: 0)
-counts.update(Counter(stack))
-i = 0
-while len(stack) > 1:
-    i += 1
-    print(i)
-    substitute_and_count(0)
+pairs = {}
+for i in range(len(template) - 1): pairs.update({template[i:i + 2]: pairs.get(template[i:i + 2], 0) + 1})
+for step in range(40):
+    new_pairs = {}
+    for pair, val in pairs.items():
+        if pair in substitutions:
+            sub = substitutions[pair]
+            pair_one = pair[0] + sub
+            pair_two = sub + pair[1]
+            new_pairs.update({pair_one: new_pairs.get(pair_one, 0) + val, pair_two: new_pairs.get(pair_two, 0) + val})
+    pairs = new_pairs
 
-print(max(counts.values()) - min(counts.values()))
+counts = {template[-1]: 1}
+for pair, val in pairs.items():
+    counts[pair[0]] = counts.get(pair[0], 0) + val
+print(max(counts.values())-min(counts.values()))
